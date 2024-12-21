f5f4142db570b99f7beeda196d0e8128c663982e
Charles Caleb <charlesofficial9@gmail.com>
2024-12-21 22:50:35 +0100

    update

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -6,9 +6,7 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateFieldsDB = (err) => {
-  // const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]; // Not working
-  // console.log(value);
-  const value = err.keyValue.name; // Fix by charles
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
 
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
@@ -27,12 +25,22 @@ const handleJWTError = () =>
 const handleJWTExpiredError = () =>
   new AppError("Your token has expired! Please log in again.", 401);
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
+const sendErrorDev = (err, req, res) => {
+  // A) API
+  if (req.originalUrl.startsWith("/api")) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+
+  // B) RENDERED WEBSITE
+  console.error("ERROR 💥", err);
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong!",
+    msg: err.message,
   });
 };
 
@@ -74,34 +82,25 @@ const sendErrorProd = (err, req, res) => {
   });
 };
 
-//////////////////////////////
-/* global error handler */
-////////////////////////////////
 module.exports = (err, req, res, next) => {
-  console.log(err.stack, "===❌❌===");
+  // console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
   if (process.env.NODE_ENV === "development") {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV.trim() === "production") {
-    //fix by charles
-    let error = { ...err }; // hard copy
-    // console.log(error.code);
+    let error = { ...err };
+    error.message = err.message;
 
-    console.log(error, "❌❌❌");
-    if (error.name === "CastError" || err.kind === "ObjectId")
-      error = handleCastErrorDB(error); // Fix by Charles
+    if (error.name === "CastError") error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (
-      error.name === "ValidationError" ||
-      error._message === "Validation failed"
-    )
-      error = handleValidationErrorDB(error); // Fix by Charles
-    if (error.name === "JsonWebTokenError") error = handleJWTError(); //inavail web token
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
     if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
 
-    // sendErrorProd(error, res);
+    sendErrorProd(error, req, res);
   }
 };
