3e044a2ba4e4d0cad1fd2717453ae7f89d8945ee
Charles Caleb <charlesofficial9@gmail.com>
2025-07-07 07:18:05 +0100

    update

--- src/utils/FileUploads.js
+++ src/utils/FileUploads.js
@@ -2,7 +2,6 @@ const fileService = require("../services/fileService");
 
 // Helper method for file uploads
 const handleFileUploads = async ({ files, documentId, modelTable }) => {
-  console.log("❌❌===>:", files, documentId, modelTable);
   const uploadedFiles = await Promise.all(
     files.map((file) =>
       fileService.uploadFile({
